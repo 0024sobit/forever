a9b60311a7ec46b2cb3f8bb37ec934c36c331531
0024sobit <0024sobitvft@gmail.com>
2025-09-06 13:27:55 +0530

    .

--- forever/src/Pages/About.jsx
+++ forever/src/Pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../Components/Title'
 import { assets } from '../assets/assets'
+import NewsLetterBox from '../Components/NewsLetterBox'
 
 const About = () => {
   return (
@@ -9,14 +10,32 @@ const About = () => {
         <Title text1={'ABOUT'} text2={'US'} /> 
       </div>
       <div className='my-10 flex flex-col md:flex-row gap-16'>
-        <img className='w-full md:mx-w-[450px] ' src={assets.about_img} alt="" />
-        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+        <img className='w-full md:mx-w-[450px] ' src={assets.about_img} alt="" />  
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'> 
           <p>Forever wan born out of a passion for inovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a p;atform where customers can easily discover , explore and purchase a wide range of products fromt eh confort of their homes</p>
           <p>Since our inception, we've worked tirelessly to curate a diverse selection of high-quality products that cater to every taste and preference. From fashion and beauty to electronics and home essentials, we offer an extensive collection sourced from trusted brands and supplies.</p>   
           <b className='text-gray-700'>Our Mission</b>
           <p>Our mission at Forever is to empower customers with choice, convenience, and confidence, We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and boyond.</p>
         </div>
       </div>
+      <div className='text-xl py-4 '>
+        <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div> 
+      <div className='flex flex-col md:flex-row text-sm mb-20'>  
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>We maticulously select and vet each product to ensure it meets out stringent quality standerds.</p> 
+        </div>
+        <div className='border border-t-0  px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gray-600'>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p> 
+        </div>
+        <div className='border border-t-0  px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>  
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>Our team of dedicated professibonals is here  to assist you the way, ensuring your satisfaction is our top priority. </p> 
+        </div>
+      </div>
+      <NewsLetterBox />
     </div>
   )
 }
